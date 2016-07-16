5d2989b1761decaf96fb532b603edcdcfe8bc1d3
ikki407 <IkkiTanaka@Ikki-Tanakano-MacBook-Pro.local>
2016-07-16 16:00:47 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='stacking',
+      version='0.0.1',
+      description='Python stacking for ensemble learning',
+      author='ikkitanaka',
+      author_email='ikki0407@gmail.com',
+      packages=find_packages(),
+      )
